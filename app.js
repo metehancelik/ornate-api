e6a98a77bcf97401945d3f03021af6a17a699879
Metehan Celik <80481282+metehancelik@users.noreply.github.com>
2022-12-18 18:36:25 +0300

    Fix cors

--- app.js
+++ app.js
@@ -21,11 +21,7 @@ if (process.env.NODE_ENV === "development") {
     app.use(morgan("dev"))
 }
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://ornateapp.netlify.app/"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+app.use(cors())
 
 
 
