1165cbb54d1c14affdae35bb1f6e332b92c69504
Metehan Celik <80481282+metehancelik@users.noreply.github.com>
2022-12-28 00:23:20 +0300

    Update authService.js

--- services/authService.js
+++ services/authService.js
@@ -73,6 +73,7 @@ const createSession = async (req, user) => {
     // create a cookie expiry date in compatible w jwt lifetime
     const expiry = new Date(Date.now() + 24 * 60 * 60 * 1000 * +process.env.JWT_SESSION_EXPIRY.slice(0, -1))
     const config = {
+        domain: 'https://adminornate.com',
         expires: expiry,
         httpOnly: true,
         secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
