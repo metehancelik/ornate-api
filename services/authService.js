63af153d326058c88e2a636364a99d60c29643a0
Metehan Celik <80481282+metehancelik@users.noreply.github.com>
2022-12-29 01:02:17 +0300

    Update authService.js

--- services/authService.js
+++ services/authService.js
@@ -73,8 +73,8 @@ const createSession = async (req, user) => {
     // create a cookie expiry date in compatible w jwt lifetime
     const expiry = new Date(Date.now() + 24 * 60 * 60 * 1000 * +process.env.JWT_SESSION_EXPIRY.slice(0, -1))
     const config = {
-        // expires: expiry,
-        // sameSite: 'none',
+        expires: expiry,
+        sameSite: 'lax',
         secure: true
     }
     return { session, config }
